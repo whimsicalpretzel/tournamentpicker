e4242d6d35a92d9add7d272a46e885b2e593bb1c
whimsicalpretzel <lcperkins@protonmail.com>
2024-01-21 16:39:36 -0500

    Added more data to data.js and made small wording correction to index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function getPlaylist() {
     const randNum = Math.floor(Math.random() * dgData.length)
     displayPlaylist.innerHTML = `
         <a href="${dgData[randNum].link}" target="_blank" id="picture-link"> <img src="${dgData[randNum].thumbnail}" alt="${dgData[randNum].year} ${dgData[randNum].tournament}"> </a>
-        <p id="tournament-name">The ${dgData[randNum].year} ${dgData[randNum].tournament}</p>
+        <p id="tournament-name">Watch the ${dgData[randNum].year} ${dgData[randNum].tournament}</p>
         <p id="prod-company">Produced by: ${dgData[randNum].productionCompany}</p>
         <p id="field-presented">This playlist features players from ${dgData[randNum].field}.</p>
         <a href="${dgData[randNum].link}" id="playlist-link" target="_blank">Watch it here</a>
